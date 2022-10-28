4a95cab1e0b50eb9adb4a0a0a9cdf491a7357cb0
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2022-10-28 22:40:55 +0300

    Update pixabayapi.js

--- src/pixabayapi.js
+++ src/pixabayapi.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 export { pixabayApiImages };
 
-const axios = require('axios');
+// const axios = require('axios');
 
 async function pixabayApiImages (name, page, perPage) {
   const url = 'https://pixabay.com/api/';
   const key = '30904237-89ef4380cd88db989fbe73792';
   perPage = 40;
   try {
-    const response = await axios.get(`${url}?key=${key}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`);
+    const response = await axios(`${url}?key=${key}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`);
     return response.data;
   } catch (error) {
     console.error(error);
